dfe4a719a255f948f73a8cf446322f44ede2c787
NhanNhan <caotheanh945@gmail.com>
2024-02-25 20:59:27 +0700

    Full Project

---
+++ DoAnPhanMem/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using DoAnPhanMem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.Entity;
+using DoAnPhanMem;
+
+
+namespace DoAnPhanMem.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Admin/Base
+        public BaseController()
+        {
+                var user = Session["TaiKhoan"] as Account;
+                if (user.Role.role_name != "Admin" || user.Role.role_name != "Nhân viên")
+                {
+                    System.Web.HttpContext.Current.Response.Redirect("~/Home/Index");
+                }
+        }
+        //đăng xuất admin quay về trang chủ
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return Redirect("~/Home/Index");
+        }
+        //chuyển từ trang admin sang trang thông tin cá nhân
+        public ActionResult ViewProfile()
+        {
+           
+            return Redirect("~/Account/Editprofile");
+        }
+        //chuyển từ trang admin sang trang chủ
+        public ActionResult BackToHome()
+        {
+
+            return Redirect("~/Home/Index");
+
+        }
+    }
+}
---
+++ DoAnPhanMem/Scripts/UserJS/accounts.js
@@ -0,0 +1,89 @@
+﻿//2. Update avatar
+function uploadFile(event) {
+    var input = document.getElementById("file-ip-1");
+    file = input.files[0];
+    if (file != undefined) {
+        formData = new FormData();
+        if (!!file.type.match(/image.*/)) {
+            formData.append("image", file);
+            $.ajax({
+                url: "Account/UpdateAvatar",
+                type: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (result) {
+                    var src = URL.createObjectURL(event.target.files[0]);
+                    var preview = document.getElementById("file-ip-1-preview");
+                    preview.src = src;
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 2000,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Cập nhật Avatar thành công'
+                    })
+                }
+            });
+        } else {
+            alert('Not a valid image!');
+        }
+    } else {
+        alert('Input something!');
+    }
+};
+
+$(".js-cancel").click(function (ev) {
+    Swal.fire({
+        title: 'Xác nhận hủy đơn hàng?',
+        text: 'Bạn có chắc chắn muốn hủy đơn hàng này?',
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#3085d6',
+        cancelButtonText: 'Hủy',
+        confirmButtonText: 'Xác nhận!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            var orderId = $(ev.currentTarget).data("id");
+
+            // Gọi hàm AJAX để gửi yêu cầu hủy đơn hàng
+            $.ajax({
+                url: "/Account/CancelOrder",
+                type: "POST",
+                data: { Oderid: orderId },
+                success: function (data) {
+                    if (data) {
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 1500,
+                            didOpen: (toast) => {
+                                toast.addEventListener('mouseenter', Swal.stopTimer)
+                                toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                        });
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Đã hủy đơn hàng thành công'
+                        });
+                    } else {
+                        Swal.fire('Không thể hủy đơn hàng', '', 'error');
+                    }
+                },
+                error: function () {
+                    Swal.fire('Có lỗi xảy ra khi gửi yêu cầu', '', 'error');
+                }
+            });
+        }
+    });
+});
